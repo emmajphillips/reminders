d87ef93298c0510085e505c940bfd07ca5048172
emmajphillips <emmjphillips@gmail.com>
2020-07-25 09:56:25 +0100

    rough layout of TasksToday component and start of custom checkboxes

--- frontend/src/components/tasks/TasksToday.js
+++ frontend/src/components/tasks/TasksToday.js
@@ -5,12 +5,19 @@ import PageContainer from '../common/PageContainer'
 import { tasks } from '../../lib/tasks'
 
 function TasksToday () {
+
+  const handleCheck = () => {
+    console.log('clicked!')
+  }
+
   return (
     <>
       <h1>Tasks</h1>
       <PageContainer>
         {tasks.map(task => (
-          <div key={task.id}>
+          <div key={task.id} className="task">
+            <input type="checkbox" />
+            <span className="checkbox" onClick={handleCheck}></span>
             <h2>{task.name}</h2>
           </div>
         ))}
