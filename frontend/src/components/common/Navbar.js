db1164bf96ffb11db08de7388b9f062ff57bee1d
emmajphillips <emmjphillips@gmail.com>
2020-07-26 11:57:02 +0100

    updated navbar styling

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -3,19 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <h2>To do app</h2>
-      <nav>
-        <ul>
-          <li><Link to="/login">Log in</Link></li>
-          <li><Link to="/signup">Sign up</Link></li>
-        </ul>
-        <span></span>
-        <span></span>
-        <span></span>
-      </nav>
-    </div>
-
+    <nav>
+      <ul>
+        <li><Link to="/login">Log in</Link></li>
+        <li><Link to="/signup">Sign up</Link></li>
+      </ul>
+      <span></span>
+      <span></span>
+      <span></span>
+    </nav>
   )
 }
 
