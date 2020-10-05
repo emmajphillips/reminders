1e15b8c4a635af4d2fbd317c3893ebf0295bc2a2
emmajphillips <emmjphillips@gmail.com>
2020-10-05 12:09:40 +0100

    adding CRUD actions for tasks

--- reminders-api/config/routes.js
+++ reminders-api/config/routes.js
@@ -3,4 +3,10 @@ const tasks = require("../controllers/tasks");
 
 router.route("/tasks").get(tasks.index).post(tasks.create);
 
+router
+  .route("/tasks/:id")
+  .get(tasks.show)
+  .put(tasks.update)
+  .delete(tasks.delete);
+
 module.exports = router;
--- reminders-api/controllers/tasks.js
+++ reminders-api/controllers/tasks.js
@@ -2,7 +2,7 @@ const Task = require("../models/task");
 
 async function tasksIndex(req, res) {
   try {
-    const tasks = await Task.find();
+    let tasks = await Task.find();
     res.status(200).json(tasks);
   } catch (err) {
     res.status(404).json(err);
@@ -11,14 +11,51 @@ async function tasksIndex(req, res) {
 
 async function taskCreate(req, res) {
   try {
-    const createdTask = await Task.create(req.body);
+    let createdTask = await Task.create(req.body);
     res.status(201).json(createdTask);
   } catch (err) {
     res.status(422).json(err);
   }
 }
 
+async function taskShow(req, res) {
+  let taskId = req.params.id;
+  try {
+    let task = await Task.findById(taskId);
+    if (!task) throw new Error();
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+}
+
+async function taskUpdate(req, res) {
+  let taskId = req.params.id;
+  try {
+    let updatedTask = await Task.findByIdAndUpdate(taskId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(202).json(updatedTask);
+  } catch (err) {
+    res.status(422).json(err);
+  }
+}
+
+async function taskDelete(req, res) {
+  let taskId = req.params.id;
+  try {
+    await Task.findByIdAndDelete(taskId);
+    res.status(202).json({ message: "Task deleted" });
+  } catch (err) {
+    res.json(err);
+  }
+}
+
 module.exports = {
   index: tasksIndex,
   create: taskCreate,
+  show: taskShow,
+  update: taskUpdate,
+  delete: taskDelete,
 };
