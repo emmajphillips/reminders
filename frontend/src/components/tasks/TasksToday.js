fde329f81e9b82e51a8459c4a4f64f90d1df8657
emmajphillips <emmjphillips@gmail.com>
2021-03-19 16:28:35 +0800

    Connect frontend with api

--- frontend/src/components/tasks/TasksToday.js
+++ frontend/src/components/tasks/TasksToday.js
@@ -1,80 +1,96 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
 
-import PageContainer from '../common/PageContainer'
-import { homePageTasks } from '../common/Home'
+import PageContainer from "../common/PageContainer";
 
-const JSONtasks = localStorage.getItem('tasks')
-const realTasks = JSON.parse(JSONtasks)
+function TasksToday() {
+  const [tasks, setTasks] = React.useState([]);
+  const [addTask, setAddTask] = React.useState(false);
+  const [newTask, setNewTask] = React.useState(null);
 
-function TasksToday () {
+  const JSONtasks = localStorage.getItem("tasks");
+  const realTasks = JSON.parse(JSONtasks);
 
-  const [tasks, setTasks] = React.useState(realTasks ? realTasks : homePageTasks)
-  const [addTask, setAddTask] = React.useState(false)
-  const [newTask, setNewTask] = React.useState('')
+  const getTasks = async () => {
+    try {
+      const res = await axios.get("/api/tasks");
+      const combinedTasks = realTasks
+        ? [...realTasks, ...res.data]
+        : [...res.data];
+      setTasks(combinedTasks);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  useEffect(() => {
+    getTasks();
+  }, [realTasks]);
 
   const handleCheck = () => {
-    console.log('clicked!')
-  }
+    console.log("clicked!");
+  };
 
   const openNewTaskForm = () => {
-    setAddTask(true)
-  }
+    setAddTask(true);
+  };
 
-  const handleChange = event => {
-    const taskToAdd = event.target.value
-    setNewTask(taskToAdd)
-  }
+  const handleChange = (event) => {
+    const taskToAdd = { completed: false, name: event.target.value };
+    setNewTask(taskToAdd);
+  };
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    if (!newTask) return
-    setTasks([...tasks, newTask])
-    localStorage.setItem('tasks', JSON.stringify([...tasks, newTask]))
-    setNewTask('')
-    setAddTask(false)
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newTask === null) return;
+    tasks.push(newTask);
+    setTasks(tasks);
+    localStorage.setItem(
+      "tasks",
+      JSON.stringify(realTasks ? [...realTasks, newTask] : [newTask])
+    );
+    setNewTask(null);
+    setAddTask(false);
+  };
 
   return (
     <>
-<<<<<<< HEAD
       <div className="header">
         <h1>Tasks</h1>
         <p>Today</p>
       </div>
-=======
-      <h1>Tasks</h1>
->>>>>>> 655e4c3... Bump websocket-extensions from 0.1.3 to 0.1.4 in /frontend
       <PageContainer>
-        {tasks.length === 0 ? 
+        {tasks.length === 0 ? (
           <h2> no tasks</h2>
-          :
+        ) : (
           tasks.map((task, index) => (
             <div key={index} className="task">
-              <input type="checkbox" className="checkbox" onClick={handleCheck}/>
-              <h2>{task}</h2>
+              <input
+                type="checkbox"
+                className="checkbox"
+                onClick={handleCheck}
+              />
+              <h2>{task.name}</h2>
             </div>
-          ))}
-        {addTask ? <div>
-          <form onSubmit={handleSubmit}>
-            <input 
-              type="text" 
-              value={newTask}
-              onChange={handleChange} 
-            />
-          </form>
-        </div> 
-          : 
-<<<<<<< HEAD
-          null}
-        <button type="button" onClick={openNewTaskForm} className="pink-button">{tasks.length === 0 ? 'add task' : 'create another'}</button>
-=======
-          <div></div>}
-        <button type="button" onClick={openNewTaskForm}>{tasks.length === 0 ? 'Add task' : 'create another'}</button>
->>>>>>> 655e4c3... Bump websocket-extensions from 0.1.3 to 0.1.4 in /frontend
+          ))
+        )}
+        {addTask ? (
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={newTask === null ? "" : newTask.name}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+        ) : null}
+        <button type="button" onClick={openNewTaskForm} className="pink-button">
+          {tasks.length === 0 ? "add task" : "create another"}
+        </button>
       </PageContainer>
     </>
-  )
+  );
 }
 
-export default TasksToday
+export default TasksToday;
